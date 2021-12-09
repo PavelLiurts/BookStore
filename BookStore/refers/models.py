c8748f999ba10093f8a514aec685451d27a1a09f
unknown <pavel.build@gmail.com>
2021-12-09 21:05:05 +0300

    add refers models

--- BookStore/refers/models.py
+++ BookStore/refers/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+class Author(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
 
+    def __str__(self):
+        return self.name
+
+class Serie(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
