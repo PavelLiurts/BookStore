50e759cf122977dee87481a511ee9b3706d431e7
Pavel <pavel.build@gmail.com>
2022-05-21 14:50:33 +0300

    feat:add book admin

--- BookStore/book/admin.py
+++ BookStore/book/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from book.models import Book
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+class BookResource(resources.ModelResource):
+    class Meta:
+        model = Book
+        fields = (
+            'id',
+            'description',
+            'price',
+            'authors',
+            'serie',
+            'genres',
+            'year_publishing',
+            'pages',
+            'binding',
+            'format',
+            'isbn',
+            'weight',
+            'age_restrictions',
+            'publishing_house',
+            'quantity',
+            'rating',
+            'cataloging',
+            'last_change',
+            'available_order'
+        )
+
+    class BookAdmin(ImportExportModelAdmin):
+        resource_class = BookResource
