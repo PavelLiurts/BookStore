8738c7d4e9db07b4fe48c593a6c1a58219d3fde3
Pavel <pavel.build@gmail.com>
2022-05-21 15:49:33 +0300

    feat:add book views

--- BookStore/book/urls.py
+++ BookStore/book/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from book.views import BookListView, BooksDetailView, BooksCreateView, BooksUpdateView, BooksDeleteView \
-    BooksPopularView, BooksNewView
 
 app_name = "book"
 
@@ -10,7 +9,4 @@ urlpatterns = [
     path('create/', BooksCreateView.as_view(), name = 'book_create'),
     path('update/<int:pk>/', BooksUpdateView.as_view(), name = 'book_update'),
     path('delete/<int:pk>/', BooksDeleteView.as_view(), name = 'book_delete'),
-    path('popular/', BooksPopularView.as_view(), name = 'book_popular'),
-    path('new/', BooksNewView.as_view(), name = 'book_new'),
-
 ]
--- BookStore/book/views.py
+++ BookStore/book/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from book.models import Book
+from book.forms import BooksCreateForm, BooksUpdateForm, BooksDeleteForm
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
-# Create your views here.
+class BooksListView(ListView):
+    template_name = 'templates/BooksList.html'
+    model = Book
+
+class BooksDetailView(DetailView):
+    template_name = 'templates/detail.html'
+    model = Book
+
+class BookCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    permission_required = 'book.add_book'
+    model = Book
+    form_class = BooksCreateForm
+    template_name = 'templates/create.html'
+    success_url = '/books'
+
+class BooksUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    permission_required = 'book.change_book'
+    model = Book
+    form_class = BooksUpdateForm
+    template_name = 'templates/update.html'
+    success_url = '/books'
+
+class BooksDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+    permission_required = 'book.delete_book'
+    model = Book
+    form_class = BooksDeleteForm
+    template_name = 'templates/delete.html'
+    success_url = 'books'
+
+
