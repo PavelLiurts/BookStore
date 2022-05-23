f53e9a98140da1aed245a76c3005b5ac7159225e
Pavel <pavel.build@gmail.com>
2022-05-23 22:35:12 +0300

    feat:add refers admin

--- BookStore/refers/admin.py
+++ BookStore/refers/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from refers.models import Author, Serie, Genre
+
+admin.site.register(Author)
+admin.site.register(Serie)
+admin.site.register(Genre)
